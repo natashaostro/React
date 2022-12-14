85a2372e63a0d42fd64d44c02ce7b816043f9990
natasha <natashaostro@hotmail.com>
2022-12-14 19:13:30 -0300

    nati-de-app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Footer from "./componentes/Footer";
 import ItemListContainer from "./componentes/ItemListContainer";
 import NavBar from "./componentes/NavBar";
 import PromocionApp from "./componentes/PromocionApp";
+import Props from "./componentes/Props";
+import States from "./componentes/States";
+
+
 
 function App (){
   return(
@@ -10,8 +14,12 @@ function App (){
     <NavBar/>
     <ItemListContainer greeting={"Trabajamos para garantizar un acceso democrático y federal a los documentos que hacen a la historia, la memoria y los derechos del Pueblo argentino."}/>
     <PromocionApp/>
+    <Props mensaje1={"hola coder"} mensaje2= {"bienvenidosss"}/>
+    <States/>
     <Footer/>
 </div>
-  )
+  );
 }
+ //componente props//
 export default App;
+
